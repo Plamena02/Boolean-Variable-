8db26d018bae9898f5cbf7ba844455b1a6de7c34
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-04-13 13:15:24 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            if (a=="True")
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
